16d5a7f6a3e55c86923e40f8660fbd4d2b12b054
Nuno Trocado <ntrocado@gmail.com>
2020-09-02 15:37:05 +0100

    Add test score

--- application.lisp
+++ application.lisp
@@ -1,12 +1,35 @@
 ;; TODO: duncan@bayne.id.au: make the Buildpack aware of the app package name
 (in-package #:cl-user)
 
+(defparameter *lilypond*
+  #+windows "C:/Program Files (x86)/LilyPond/usr/bin/lilypond.exe"
+  #+linux "/app/.apt/usr/bin/lilypond.real")
+
+(defparameter *test-score*
+  "\\score{
+	{
+		c'
+	}
+
+	\\layout{}
+	\\midi{}
+}")
+
 (hunchentoot:define-easy-handler (root :uri "/") ()
-  (cl-who:with-html-output-to-string (s nil :prologue t)
-    (:html
-      (:body
-        (:p "hello, world")
-        (:img :src "lisp-logo120x80.png")))))
+  (setf (hunchentoot:content-type*) "application/pdf")
+  (let* ((output-filename #+linux "/app/output"
+			  #+windows "C:/Users/trocado/Desktop/output")
+	 (output-file (make-pathname :type "pdf" :defaults output-filename)))
+    (uiop:with-temporary-file (:stream stream :pathname input-file)
+      (format stream *test-score*)
+      :close-stream
+      (uiop:run-program (list *lilypond*
+			      "-o"
+			      output-filename
+			      (namestring input-file))))
+    (prog1
+	(alexandria:read-file-into-byte-vector output-file)
+      (uiop:delete-file-if-exists output-file))))
 
 (defvar *acceptor* nil)
 
